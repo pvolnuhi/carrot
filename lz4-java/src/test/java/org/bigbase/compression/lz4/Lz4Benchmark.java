4a36be460edd42a51ae4acdb61f947c9076f4797
Vladimir Rodionov <vladrodionov@gmail.com>
2020-06-25 16:32:43 -0700

    LZ4 compression benchmark added

---
+++ lz4-java/src/test/java/org/bigbase/compression/lz4/Lz4Benchmark.java
@@ -0,0 +1,96 @@
+package org.bigbase.compression.lz4;
+import java.lang.reflect.Field;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.Random;
+
+import org.junit.Test;
+
+import sun.misc.Unsafe;
+
+public class Lz4Benchmark {
+  int blockSize = 4096;
+  long numBlocks = 500000;
+  Random r = new Random();
+  static Unsafe unsafe;
+  static {
+    unsafe = (Unsafe) AccessController.doPrivileged(new PrivilegedAction<Object>() {
+      @Override
+      public Object run() {
+        try {
+          Field f = Unsafe.class.getDeclaredField("theUnsafe");
+          f.setAccessible(true);
+          return f.get(null);
+        } catch (Throwable e) {
+          System.err.println("sun.misc.Unsafe is not accessible: " + e.getMessage());
+        }
+        return null;
+      }
+    });
+  }
+
+  @Test
+  public void testBlocksCompressDecompressSequentially() {
+    
+    System.out.println("testBlocksCompressDecompressSequentially");
+    long address = unsafe.allocateMemory(blockSize * numBlocks);
+    fillBlocks(address);
+    long buf = unsafe.allocateMemory(blockSize);
+    for (int k = 0; k < 10; k++) {
+      System.out.println("iteration "+(k+1));
+      long startTime = System.currentTimeMillis();
+      for (int i=0; i < numBlocks; i++) {        
+        long src = address + i * blockSize;
+        int r = LZ4.compressDirectAddress(src, blockSize, buf, blockSize);
+        int size = LZ4.decompressDirectAddress(buf, r, src, blockSize);
+      }
+      long endTime = System.currentTimeMillis();
+      System.out.println(numBlocks * 1000/ (endTime -startTime) +" block comp/decomp per sec");
+    }
+    unsafe.freeMemory(address);
+    unsafe.freeMemory(buf);
+  }
+  
+  @Test
+  public void testBlocksCompressDecompressRandomly() {
+    System.out.println("testBlocksCompressDecompressRandomly");
+
+    long address = unsafe.allocateMemory(blockSize * numBlocks);
+    fillBlocks(address);
+    long buf = unsafe.allocateMemory(blockSize);
+    for (int k = 0; k < 10; k++) {
+      System.out.println("iteration "+(k+1));
+      long startTime = System.currentTimeMillis();
+      for (int i=0; i < numBlocks; i++) {
+        int n =  r.nextInt((int)numBlocks);
+        long src = address + n * blockSize;
+        int r = LZ4.compressDirectAddress(src, blockSize, buf, blockSize);
+        int size = LZ4.decompressDirectAddress(buf, r, src, blockSize);
+      }
+      long endTime = System.currentTimeMillis();
+      System.out.println(numBlocks * 1000/ (endTime -startTime) +" block comp/decomp per sec");
+    }
+    unsafe.freeMemory(address);
+    unsafe.freeMemory(buf);
+  }
+  
+  private void fillBlocks(long startPtr) {
+    // Block size is 'blockSize'
+    System.out.println("fill start");
+    long ptr = startPtr;
+    long endPtr = startPtr + blockSize * numBlocks;
+    
+    while (ptr < endPtr) {
+      long l1 = r.nextLong();
+      long l2 = r.nextLong();
+      unsafe.putLong(ptr, l1);
+      unsafe.putLong(ptr + 8, l2);
+      unsafe.putLong(ptr + 16, 0L);
+      unsafe.putLong(ptr + 24, 0L);
+      ptr += 32;
+    }
+    System.out.println("fill finished");
+
+  }
+  
+}
